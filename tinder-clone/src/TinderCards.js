7395ac8922f1a092fb8d6c860c6612f6e5019f8c
Jpduker <jayaprakashnis619@gmail.com>
2023-01-11 21:43:33 +0530

    added Icons

---
+++ tinder-clone/src/TinderCards.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import "./TinderCards.css"
+
+function TinderCards() {
+
+    const[people , setPeople] =useState([
+        name : "Elon Musk",
+
+    ])
+  return (
+    <div>TinderCards</div>
+  )
+}
+
+export default TinderCards
