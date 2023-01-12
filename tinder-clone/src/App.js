72273c92b3e74a011ccac52d826945dc538d64d5
Jpduker <jayaprakashnis619@gmail.com>
2023-01-12 23:42:09 +0530

    added swipped, outOfFrame functionalities

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import './App.css';
 import Headers from './Headers';
+import TinderCards from 'react-tinder-card';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
    
 
     <Headers />
-    {/* TinderCards */}
+    // <TinderCards />
     {/* SwipeButtons */}
 
     </div>
