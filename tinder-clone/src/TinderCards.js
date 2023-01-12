1db6c6e375c57d1687a8fb8942f9f0012973638f
Jpduker <jayaprakashnis619@gmail.com>
2023-01-12 23:23:38 +0530

    update

--- tinder-clone/src/TinderCards.js
+++ tinder-clone/src/TinderCards.js
@@ -1,28 +1,29 @@
-import { Person } from '@mui/icons-material'
-import React, { useState } from 'react'
-import TinderCard from 'react-tinder-card'
-import "./TinderCards.css"
+import { Person } from "@mui/icons-material";
+import React, { useState } from "react";
+import TinderCard from "react-tinder-card";
+import "./TinderCards.css";
 
 function TinderCards() {
+	const [people, setPeople] = useState([
+		{ data: "Elon Musk" },
+		{ data: "Jeff Bezos" },
+	]);
 
-    const[people , setPeople] =useState([
-        {data : "Elon Musk"},
-        {data: "Jeff Bezos"}
-
-    ])
-  return (
-    <div className='TinderCards'>
-      <div className='tinderCards__cardsContainer'>
-      {people.map((Person)=>(
-        <TinderCard>
-        
-        </TinderCard>
-      ))}
-      
-      </div>
-       
-    </div>
-  )
+	return (
+		<div className="TinderCards">
+			<div className="tinderCards__cardsContainer">
+				{people.map((person) => (
+					<TinderCard
+						className="swipe"
+						key={person.data}
+						preventSwipe={["up", "down"]}
+						onSwipe={(dir) => (dir, person.data)}
+						onCardLeftScreen={() => outofFrame(person.data)}
+					></TinderCard>
+				))}
+			</div>
+		</div>
+	);
 }
 
-export default TinderCards
+export default TinderCards;
