c8acb9a2ebafc2c3c4ca38f27c05b4ff440a1a97
Jpduker <jayaprakashnis619@gmail.com>
2023-01-13 23:17:41 +0530

    added swipe buttons and style

--- tinder-clone/src/SwipeButons.js
+++ tinder-clone/src/SwipeButons.js
@@ -4,25 +4,30 @@ import './SwipeButtons.css'
 import CloseIcon from '@mui/icons-material/Close';
 import StarRateIcon from '@mui/icons-material/StarRate';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import { IconButton } from '@mui/material'
+import IconButton from '@mui/material/IconButton';
+
 import FlashOnIcon from '@mui/icons-material/FlashOn';
 function SwipeButons() {
   return (
     <div className='swipeButtons'>
     <IconButton className='swipeButtons_repeat' >
-        <ReplayIcon fontSize='large ' />
+        <ReplayIcon fontSize='large' />
     </IconButton>
+
     <IconButton className='swipeButtons_left' >
-    <CloseIcon fontSize='large ' />
+        <CloseIcon fontSize='large' />
     </IconButton>
+
     <IconButton className='swipeButtons_star' >
-    <StarRateIcon fontSize='large ' />
+        <StarRateIcon fontSize='large' />
     </IconButton>
+
     <IconButton className='swipeButtons_right' >
-    <FavoriteIcon fontSize='large ' />
+        <FavoriteIcon fontSize='large' />
     </IconButton>
+
     <IconButton className='swipeButtons_lightning' >
-    <FlashOnIcon fontSize='large ' />
+        <FlashOnIcon fontSize='large' />
     </IconButton>
     
     </div>
