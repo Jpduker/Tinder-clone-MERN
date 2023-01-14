15979cbd12eb2fcf4b829c6434811dc765b2af2d
Jpduker <jayaprakashnis619@gmail.com>
2023-01-14 23:03:27 +0530

    added BD Config

--- tinder-backend/server.js
+++ tinder-backend/server.js
@@ -4,10 +4,16 @@ import mongoose from "mongoose";
 //App Config
 const app = express();
 const port =process.env.PORT || 8001;
+const connection_url ="mongodb+srv://jpduker:Jpduker390@cluster0.kr2e0tb.mongodb.net/?retryWrites=true&w=majority"
 
 //Middlewares
 
 //DB config
+mongoose.connect(connection_url,{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopology:true,
+})
 
 //API Endpoints
 app.get('/',(req,res) => {
