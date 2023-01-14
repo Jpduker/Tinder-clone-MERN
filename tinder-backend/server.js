b22cad9bcf3dc1beafa8e49c6d2f13d8d04abf3a
Jpduker <jayaprakashnis619@gmail.com>
2023-01-14 23:20:37 +0530

    added tinder/card API endpoint

--- tinder-backend/server.js
+++ tinder-backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
-
+import Cards from "./dbCards"
 //App Config
 const app = express();
 const port =process.env.PORT || 8001;
@@ -18,6 +18,19 @@ mongoose.connect(connection_url,{
 //API Endpoints
 app.get('/',(req,res) => {
     res.status(200).send("Hello MERN stack")
+});
+
+app.post('tinder/card',(req,res) =>{
+    const dbCard =req.body
+ 
+    Cards.create(dbCard,(err,data)=>{
+        if(err){
+            res.status(500).send(err)
+        }else{
+            res.status(201).send(data)
+        }
+            
+    })
 });
 
 //Listner
